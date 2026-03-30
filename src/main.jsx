7f5cacfcdb3797b4b355c733ef0563bc40644315
Michael Rice <michael@michaelnextleveldevelopment.com>
2026-03-30 15:06:35 -0700

    Poppersworld initial deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Poppersworld from './Poppersworld.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Poppersworld />
+  </React.StrictMode>
+);
